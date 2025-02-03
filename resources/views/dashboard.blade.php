1df5264fd6252e19ef788c974efbdf937dc6cd91
yeswanth-svg <130365668+yeswanth-svg@users.noreply.github.com>
2025-02-03 15:49:12 +0530

    second commit

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,30 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('title', 'Dashboard')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
+@section('content')
+<!-- Hero Start -->
+<div class="container-fluid py-6 my-6 mt-0" style="
+        background: url('img/bg-cover.jpg') no-repeat center center/cover;
+        color: white;">
+    <div class="container text-center animated bounceInDown">
+        <h1 class="display-1 mb-4" style="color: white">Menu</h1>
+        <ol class="breadcrumb justify-content-center mb-0 animated bounceInDown">
+            <li class="breadcrumb-item">
+                <a href="#" style="color: white">Home</a>
+            </li>
+            <li class="breadcrumb-item text-light" aria-current="page">Dashboard</li>
+        </ol>
+    </div>
+</div>
+<!-- Hero End -->
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="p-6 text-gray-900">
+                {{ __("You're logged in!") }}
             </div>
         </div>
     </div>
+</div>
 </x-app-layout>
--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -1,29 +1,42 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
-        </h2>
-    </x-slot>
+@extends('layouts.app')
+@section('title', 'Profile')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-profile-information-form')
-                </div>
+@section('content')
+<!-- Hero Start -->
+<div class="container-fluid py-6 my-6 mt-0" style="
+        background: url('img/bg-cover.jpg') no-repeat center center/cover;
+        color: white;">
+    <div class="container text-center animated bounceInDown">
+        <h1 class="display-1 mb-4" style="color: white">Menu</h1>
+        <ol class="breadcrumb justify-content-center mb-0 animated bounceInDown">
+            <li class="breadcrumb-item">
+                <a href="#" style="color: white">Home</a>
+            </li>
+            <li class="breadcrumb-item text-light" aria-current="page">Profile</li>
+        </ol>
+    </div>
+</div>
+<!-- Hero End -->
+
+<div class="py-12">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-profile-information-form')
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.update-password-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.update-password-form')
             </div>
+        </div>
 
-            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @include('profile.partials.delete-user-form')
-                </div>
+        <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+            <div class="max-w-xl">
+                @include('profile.partials.delete-user-form')
             </div>
         </div>
     </div>
-</x-app-layout>
+</div>
+@endsection
